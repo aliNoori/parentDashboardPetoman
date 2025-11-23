114e17d6a47fef1f1eaac6a505736f0d2c57f92e
Ali Noori <matineali@gmail.com>
2025-11-23 22:53:10 +0330

    set base url for api

--- src/plugins/axiosPlugins.ts
+++ src/plugins/axiosPlugins.ts
@@ -9,7 +9,7 @@ export default {
     install(app: App) {
         const instance = axios.create({
             //baseURL: 'http://127.0.0.1:3000/api',
-            baseURL: 'http://api.petoman.com/api',
+            baseURL: 'https://api.petoman.com/',
         })
 
         instance.interceptors.request.use(
